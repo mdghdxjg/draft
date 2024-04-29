0e4bf208efd007d22190bae13cb9a08a36d55dac
mdghdxjg <manno6878@gmail.com>
2024-04-29 11:50:24 +0100

    first

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './main/navbar/navbar';
+import Main from './main/main';
+
+function App() {
+  return (
+    <>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ front/src/main/main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './navbar/navbar'
+
+function Main() {
+  return (
+    <>
+      <Navbar/>
+      <div className='video'>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default Main
